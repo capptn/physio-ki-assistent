ca1dc011a62130aab0cdc1b87214b9aa915aa9be
Webdesigner Unger - Fabian Unger <info@capptn.com>
2026-03-17 12:06:46 +0000

    removed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,8 +71,6 @@ export default function RootLayout({
           </NotificationContextProvider>
         </AuthProvider>
         <PWARegister />
-
-        <Analytics />
       </body>
     </html>
   );
