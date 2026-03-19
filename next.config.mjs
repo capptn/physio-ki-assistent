fe1200d5739751c4c68f1af1b1490071fd712b11
Webdesigner Unger - Fabian Unger <info@capptn.com>
2026-03-19 10:12:23 +0000

    iframe permissons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  frame-ancestors 'self' https://bittel.cloud.capptn-online.de;
+`;
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -6,6 +10,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
